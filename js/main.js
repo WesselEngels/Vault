2be9187dbc3d5ed1df38f50991e24d926bf6dd29
Unknown <63389@rocteraa-student.nl>
2018-02-22 11:14:26 +0100

    styled the website

--- js/main.js
+++ js/main.js
@@ -1,7 +1,44 @@
-var numberContainer = document.getElementById("number-container");
+var numberContainer = document.getElementById("number-container"),
+    goodAnswer = document.getElementById("good-answer"),
+    wrongAnswer = document.getElementById("wrong-answer"),
+    clickCounter = 0,
+    numberOne,
+    numberTwo,
+    numberThree;
+
+function resetValues() {
+  numberContainer.innerHTML = "";
+  goodAnswer.innerHTML = "";
+  wrongAnswer.innerHTML = "";
+  clickCounter = 0;
+  numberOne = 0;
+  numberTwo = 0;
+  numberThree = 0;
+}
 
 function getNumber(button) {
   // console.log(button.value);
+  clickCounter++;
   numberContainer.innerHTML += button.value;
 
+  if (clickCounter == 1) {
+    numberOne = button.value;
+  } else if (clickCounter == 2) {
+    numberTwo = button.value;
+  }
+  else {
+    numberThree = button.value;
+  }
+
+  if (clickCounter == 3) {
+
+    if (numberOne == 3 && numberTwo == 1 && numberThree == 1) {
+      document.getElementById("good-answer").innerHTML = "You guessed it right!";
+      setTimeout(function(){ resetValues() }, 2500);
+    }
+    else {
+      document.getElementById("wrong-answer").innerHTML = "Wrong Code, Try Again!";
+      setTimeout(function(){ resetValues() }, 2500);
+    }
+  }
 }
