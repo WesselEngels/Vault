a64352e0cd397113f71ba45193a8777821152bd0
Unknown <63389@rocteraa-student.nl>
2018-02-28 13:15:05 +0100

    Finished my Vault

--- js/main.js
+++ js/main.js
@@ -10,6 +10,11 @@ var numberContainer = document.getElementById("number-container"),
     numberOne,
     numberTwo,
     numberThree;
+    countAttempts1 = document.getElementById("count-attempts");
+    countAttempts = 0,
+    audioWin = new Audio('snd/correctanswer.mp3');
+    audioLose = new Audio('snd/wronganswer.mp3');
+
 
 function resetValues() {
   numberContainer.innerHTML = "";
@@ -20,12 +25,14 @@ function resetValues() {
   numberTwo = 0;
   numberThree = 0;
   intervalTimer = 0;
+  document.body.style.backgroundImage = "url('img/vault2.jpg')";
   for(i=0; i < numberButtons.length; i++) {
     numberButtons[i].removeAttribute('disabled');
   }
   for(i=0; i < numberButtons1.length; i++) {
     numberButtons1[i].removeAttribute('disabled');
   }
+
 }
 
 function getNumber(button) {
@@ -35,14 +42,18 @@ function getNumber(button) {
 
   if (clickCounter == 1) {
     numberOne = button.value;
+
   } else if (clickCounter == 2) {
     numberTwo = button.value;
+
   }
   else {
     numberThree = button.value;
   }
 
   if (clickCounter == 3) {
+    countAttempts++;
+    countAttempts1.innerHTML = countAttempts;
     for(i=0; i < numberButtons.length; i++) {
       numberButtons[i].setAttribute('disabled', 'disabled');
     }
@@ -50,8 +61,11 @@ function getNumber(button) {
       numberButtons1[i].setAttribute('disabled', 'disabled');
     }
     if (numberOne == 3 && numberTwo == 1 && numberThree == 1) {
+      document.body.style.backgroundImage = "url('img/vaultopend.jpg')";
+      document.body.style.backgroundRepeat = "no-repeat";
+      document.body.style.backgroundSize = "cover";
+      audioWin.play();
       document.getElementById("good-answer").innerHTML = "You guessed it right!";
-
       var blink = setInterval(function() {
 
       	//add +1 every time the setinterval function runs
@@ -59,25 +73,29 @@ function getNumber(button) {
 
       	//method to show div on and off
       	//change the css of the green and red box to create a blinking effect
-      	if (greenBox.style.visibility == 'hidden') {
-      		greenBox.style.visibility = 'visible';
+      	if (greenBox.style.backgroundColor == 'lightgreen') {
+      		greenBox.style.backgroundColor = 'darkgreen';
       	}else {
-      		greenBox.style.visibility = 'hidden';
+      		greenBox.style.backgroundColor = 'lightgreen';
       	}
 
       	//check if the interval has runned ten times
       	if(intervalTimer == 6) {
 
-      		//ClearInterval function stops the interval after 10 times
+          countAttempts1.innerHTML = "";
+          countAttempts = 0;
+
       		clearInterval(blink);
           resetValues();
+
       	}
 
       }, 500);
+
     }
     else {
       document.getElementById("wrong-answer").innerHTML = "Wrong Code, Try Again!";
-
+      audioLose.play();
       var blink = setInterval(function() {
 
       	//add +1 every time the setinterval function runs
@@ -85,10 +103,10 @@ function getNumber(button) {
 
       	//method to show div on and off
       	//change the css of the green and red box to create a blinking effect
-      	if (redBox.style.visibility == 'hidden') {
-      		redBox.style.visibility = 'visible';
+      	if (redBox.style.backgroundColor == 'red') {
+      		redBox.style.backgroundColor = 'darkred';
       	}else {
-      		redBox.style.visibility = 'hidden';
+      		redBox.style.backgroundColor = 'red';
       	}
 
       	//check if the interval has runned ten times
@@ -103,12 +121,3 @@ function getNumber(button) {
     }
   }
 }
-
-
-
-//counter start at zero
-
-
-
-
-//counter start at zero
